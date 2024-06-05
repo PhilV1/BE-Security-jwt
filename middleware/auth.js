24c1a276fc20de1a9e9d1f183fd9453590f34c41
PhilV1 <werpub.phil@gmx.de>
2024-06-05 14:08:52 +0200

    chore: Refactor authentication middleware to handle missing token

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,8 +2,11 @@ import jwt from 'jsonwebtoken'
 import 'dotenv/config'
 
 const auth = (req, res, next) => {
-  const token =
-    req.body.token || req.query.token || req.headers.authorization.split(' ')[1]
+  const token = req.body.token || req.query.token
+
+  if (req.headers.authorization) {
+    token = req.headers.authorization.split(' ')[1]
+  }
 
   if (!token) {
     return res.status(403).send('A token is required for authentication')
